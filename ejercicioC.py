37f16ac0a11fd1f87d8b68b21dcd9c0a49ee9dca
Juan Camilo <juancami98.11@hotmail.com>
2020-04-13 21:26:03 -0500

    ejercicio PythonC

---
+++ ejercicioC.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+
+class salario(MRJob):
+
+  def mapper(self, _, line):
+    idemp, sector, salary, year = line.split(',')
+   
+    yield idemp, sector, 1
+    
+      
+  def reducer (self, idemp, sector):
+    sumaSector = 0
+
+    for sec, cont in sector:
+      sumaSector = sumaSector + cont
+       
+    yield idemp, sumaSector
+
+if __name__ == '__main__':
+  salario.run()
